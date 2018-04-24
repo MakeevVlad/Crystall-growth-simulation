2df92aaf964c4b58f657b4b140830b0902f8c5d0
Vladislav <MakeevVlad>
2018-04-24 18:07:11 +0300

    Changes are marked

--- Functions.cpp
+++ Functions.cpp
@@ -4,39 +4,66 @@
 
 #include "Functions.h"
 
+//*****
 
 
+
+
+
+//*****Фунции движения********************************************************************
 size_t direction(Molecule mol, Field field)
 {
-	size_t i =0;
-	std::vector<std::vector<double&>> pot;
-	for (size_t x = mol.x - 1; x <= mol.x + 1; ++x)
-		for (size_t y = mol.y -1; y <= mol.y + 1; ++y)
-			for (size_t z = mol.z; z <= mol.z + 1; ++z )
-		{
-		 	if (field[x][y][z][0] == 1) continue; //В простейшем случае свободная частица не может выбить уже закреплённую
 
-			pot[i][0] = field[x][y][z][1];
-			pot[i][1] = x;
-			pot[i][2] = y;
-			pot[i][3] = z;
-		}
+	std::vector<std::vector<double&>> pot;
+
+	//Sluchay, kogda chastitsa peremeshaetsa strogo po Oy
+	if (mol.dir[0] == 0)
+	{
+		//TO, kuda budet dvigatsa chasstitsa
+		if (mol.dir[1] ==  1) size_t y = y.mol + 1;
+		if (mol.dir[1] == -1) size_t y = y.mol - 1;
+
+		(pot[0][0], pot[0][1], pot[2], pot[0][3]) = (field[mol.x][mol.y][mol.z][1], mol.x, mol.y, mol.z);
+
+		for (size_t x = mol.x - 1; x <= mol.x + 1; ++x)
+			for (size_t z = mol.z - 1, size_t i = 1; z <= mol.z + 1; ++z, ++i )
+			{
+				if (field[x][y][z][0] == 1) continue; //В простейшем случае свободная частица не может выбить уже закреплённую
+				(pot[i][0], pot[i][1], pot[i][2], pot[i][3]) = (field[x][y][z][1], x, y, z);
+			}
+	}
+
+	//Sluchay, kogda chastitsa peremeshaetsa srtogo po Ox
+	if (mol.dir[0] != 0 && mol.dir[1] != 1)
+	{
+		if (mol.dir[1] ==  1) size_t x = x.mol + 1;
+		if (mol.dir[1] == -1) size_t x = x.mol - 1;
+
+		(pot[0][0], pot[0][1], pot[2], pot[0][3]) = (field[mol.x][mol.y][mol.z][1], mol.x, mol.y, mol.z);
+
+		for (size_t y = mol.x; y <= mol.y + 1; ++y)
+			for (size_t z = mol.z - 1, size_t i = 1; z <= mol.z + 1; ++z, ++i )
+			{
+				if (field[x][y][z][0] == 1) continue; //В простейшем случае свободная частица не может выбить уже закреплённую
+				(pot[i][0], pot[i][1], pot[i][2], pot[i][3]) = (field[x][y][z][1], x, y, z);
+			}
+	}
+
+	//Sluchay, kogda chastitsa dvigaetsa po diagonali
+	else
+	{
+		return 0;
+	}
+
 	//Сортировка (Допустим, что после неё вектор будет отсортирован по мервым элементам)
 	std::sort(pot.rbegin(), pot.rend());
-
-
 	//Выбор направления
 	r = (rand() % 100) / 100;
-
 	//Массив с вероятностями
-	double p[pot.size()];
-	double c = 0;
-	for (i = 0; i <= pot.size(); ++i) c += pot[i];
+	double p[pot.size() + 1];
 
-	for (i = 0; i<=pot.size(); ++i)
-	{
-		p[i] = pot[i] / c;
-	}
+	for (i = 0; i <= pot.size(); p[pot.size()] += pot[i], ++i);
+	for (i = 0; i<=pot.size();p[i] = pot[i] / p[pot.size()], ++i)
 }
 
 
