6d3197e58db244478441a0087454d13bec188627
avmmal <32042102+MakeevVlad@users.noreply.github.com>
2018-05-07 21:45:59 +0300

    Correction of [] is nessesary

--- Source.cpp
+++ Source.cpp
@@ -1,25 +1,17 @@
 #include <vector>
 #include <iostream>
-
-
-typedef std::vector<std::vector<std::vector<double>>> tdim_vec;
-
+#include "Functions.h"
 
 
 int main()
 {
-	//Substrate, field[0] -- availability of crystal germ, fiel[1] -- potencial
+
 
 	size_t x, y, z;
 
-	/*
-	while(1)
-	{
-	coord(w, l);
+	Field field;
+	Molecule mol(field);
 
-	}
-	*/
 
+	return 0;
 }
-
-
