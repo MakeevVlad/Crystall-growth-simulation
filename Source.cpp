39d0355b1c0feddc3feb1596353c6d65757dbc41
avmmal <32042102+MakeevVlad@users.noreply.github.com>
2018-05-08 01:20:12 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -8,10 +8,17 @@ int main()
 
 
 	size_t x, y, z;
+	x = 10;
+	y = 10;
+	z = 0;
 
-	Field field;
+	size_t ch;
+
+	Field field(100, 100, 100);
+	
+	ch = field.get_size_x();
 	Molecule mol(field);
-
+	//field.potencial();
 
 	return 0;
 }
