51d9ee2afb05fe54c2309548d4815c5c18d2dd50
avmmal <32042102+MakeevVlad@users.noreply.github.com>
2018-05-21 22:29:44 +0300

    Painter)

---
+++ Show.py
@@ -0,0 +1,28 @@
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+
+fig = plt.figure()
+gp = fig.add_subplot(111, projection='3d')
+gp.set_xlabel('Phi')
+gp.set_ylabel('cos(Theta)')
+gp.set_title('Efficiency')
+
+x = []  #PHI
+y = []	#TETA
+z = []	#EFF
+pot = []
+
+
+f = open('crystal.txt')
+for line in f:
+	nums = [float(i) for i in line.split()]
+	y.append(nums[0])
+	x.append(nums[1])
+	z.append(nums[2])
+	pot.append(np.abs(nums[3]))
+
+gp.scatter(x, y, z, cmap = pot)
+
+plt.show()
